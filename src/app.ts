e8f2c10eaee0c512841d2203b8cf2614bc445fff
karanvir singh <ksingh34@academic.rrc.ca>
2026-03-08 16:29:47 -0500

    fractor: register health routes in application router.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application } from "express";
+import healthRoutes from "./api/v1/routes/healthRoutes";
 import loanRoutes from "./api/v1/routes/loanRoutes";
 
 const app: Application = express();
 
 app.use(express.json());
+app.use("/api/v1", healthRoutes);
 
 app.use("/api/v1", loanRoutes);
 
