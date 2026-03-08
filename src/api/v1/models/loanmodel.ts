c7075dc63933e9441faf1fa2f5b4a9c887441d06
karanvir singh <ksingh34@academic.rrc.ca>
2026-03-07 21:54:29 -0600

    feat: defining the loan application.

---
+++ src/api/v1/models/loanmodel.ts
@@ -0,0 +1,7 @@
+export interface LoanApplication {
+    id: string;
+    applicant: string;
+    amount: number;
+    status: "pending" | "under_review" | "flagged" | "approved" | "rejected";
+    createdAt: string;
+}
