18bdded94dd84ce2ecde2ef099133c9a41e05155
karanvir singh <ksingh34@academic.rrc.ca>
2026-03-09 07:40:34 -0500

    feat: initializing the expres with logging.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Application } from "express";
+import { consoleLogger } from "./api/v1/middleware/logger";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 import loanRoutes from "./api/v1/routes/loanRoutes";
 
 const app: Application = express();
+app.use(consoleLogger);
 
 app.use(express.json());
 app.use("/api/v1", healthRoutes);
