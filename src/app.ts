9b6b217edd360fa5f3e32b3802b961547160d36e
karanvir singh <ksingh34@academic.rrc.ca>
2026-03-22 18:59:13 -0500

    chore: Connecting the Firebase authentication routes in express file.

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,8 @@ import healthRoutes from "./api/v1/routes/healthRoutes";
 import loanRoutes from "./api/v1/routes/loanRoutes";
 import { errorHandler } from "./api/v1/middleware/errorHandler";
 import { httpLogger, consoleHttpLogger } from "./api/v1/middleware/httpLogger";
-
+import userRoutes from "./api/v1/routes/userRoutes";
+import adminRoutes from "./api/v1/routes/adminRoutes";
 const app = express();
 
 app.use(httpLogger);
@@ -17,7 +18,8 @@ app.use(express.json());
 
 app.use("/api/v1/health", healthRoutes);
 app.use("/api/v1/loans", loanRoutes);
-
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/admin", adminRoutes);
 app.use(errorHandler);
 
 export default app;
